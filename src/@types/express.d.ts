5c53e453fafe5ba39cba66c0fb5d4587e1d64818
Vitor Paes <paesvitor@outlook.com>
2021-01-05 17:42:50 -0300

    Add socket

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,9 +1,10 @@
 import { User } from "@entities/User";
-
+import { Socket } from "socket.io";
 declare global {
   namespace Express {
     interface Request {
       user?: User;
+      io?: Socket;
     }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,29 @@ import express, { Request, Response, Application } from "express";
 import bodyParser from "body-parser";
 import routes from "./routes";
 import connect from "./connect";
+import cors from "cors";
+
 const app: Application = express();
 const port = 3001;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
+app.use(function (req, res, next) {
+  req.io = io;
+  next();
+});
+
+io.on("connection", (socket) => {
+  // ...
+
+  console.log("New connection");
+});
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(routes);
-app.listen(port, () =>
+http.listen(port, () =>
   console.log(`Application started successfully on port ${port}.`)
 );
 const db = "mongodb://localhost:27017/guessr";
